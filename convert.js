99074fa197d48378fb8b14d6fcdb472de1d2c707
Alex <tischlinger.alex@gmail.com>
2017-09-23 12:27:02 +0200

    complete version without testing

--- convert.js
+++ convert.js
@@ -1,16 +1,7 @@
-const LENGTH = {term:['mm', 'cm', 'dm', 'm', 'km'], multiply:[1, 10, 100, 1000, 1000000]};
-const WEIGHT = {term:['mcg', 'mg', 'g', 'kg', 't'], multiply:[1, 1000, 1000000, 1000000000, 1000000000000]};
-/*if(process.argv.LENGTH == 6 && !isNaN(process.argv[2 + 0]) && process.argv[2 + 2] === 'to' && ((LENGTH.term.includes(process.argv[2 + 1]) && LENGTH.term.includes(process.argv[2 + 3])) || (WEIGHT.term.includes(process.argv[2 + 1]) && WEIGHT.term.includes(process.argv[2 + 3]))))
-    console.log('ja');*/
+const LENGTH = {terms:['mm', 'cm', 'm'], multiply:[1, 10, 1000]};
+const WEIGHT = {terms:['g', 'kg'], multiply:[1, 1000]};
 
-
-//console.log(LENGTH.term.includes(process.argv[3]));
-//console.log(LENGTH.term.includes(process.argv[5]));
-console.log((LENGTH.term.includes(process.argv[2 + 1]) && LENGTH.term.includes(process.argv[2 + 3])) || (WEIGHT.term.includes(process.argv[2 + 1]) && WEIGHT.term.includes(process.argv[2 + 3])));
-//console.log(`${LENGTH.term.includes('mm')}\n${WEIGHT.multiply}`);
-
-// Error ausgeben von falschen Argumenten
-/*function errorOut() {
-    console.error('Invalid arguments, usage: node converter.js <float> <LENGTH, WEIGHT in short Terms> to <LENGTH, WEIGHT in short Terms>');
-    process.exit(-1);
-}*/
+if(process.argv.length == 6 && !isNaN(process.argv[2 + 0]) && process.argv[2 + 2] === 'to' && ((LENGTH.terms.includes(process.argv[2 + 1]) && LENGTH.terms.includes(process.argv[2 + 3])) || (WEIGHT.terms.includes(process.argv[2 + 1]) && WEIGHT.terms.includes(process.argv[2 + 3]))))
+    console.log(`${process.argv[2 + 0]} ${process.argv[2 + 1]} are ${parseFloat(process.argv[2]) * (LENGTH.terms.includes(process.argv[2 + 1]) ? (LENGTH.multiply[LENGTH.terms.indexOf(process.argv[2 + 1])] / LENGTH.multiply[LENGTH.terms.indexOf(process.argv[2 + 3])]) : (WEIGHT.multiply[WEIGHT.terms.indexOf(process.argv[2 + 1])] / WEIGHT.multiply[WEIGHT.terms.indexOf(process.argv[2 + 3])]))} ${process.argv[2 + 3]}`);
+else
+    console.log(`Invalid arguments, usage: node converter.js <number> <LENGTH, WEIGHT in short terms> to <LENGTH, WEIGHT in short terms>\nLength in short terms: ${LENGTH.terms}\nWeight in short terms: ${WEIGHT.terms}`);
