9147460b2f29d881b50f08a9a0ef99ff57350fbe
Alex <tischlinger.alex@gmail.com>
2017-09-23 17:09:13 +0200

    Changed username in github

--- convert.js
+++ convert.js
@@ -1,6 +1,6 @@
 const LENGTH = {terms:['mm', 'cm', 'm'], multiply:[1, 10, 1000]};
 const WEIGHT = {terms:['g', 'kg'], multiply:[1, 1000]};
-
+ 
 if(!(process.argv.length == 6 && !isNaN(process.argv[2 + 0]) && process.argv[2 + 2] === 'to' && ((LENGTH.terms.includes(process.argv[2 + 1]) && LENGTH.terms.includes(process.argv[2 + 3])) || (WEIGHT.terms.includes(process.argv[2 + 1]) && WEIGHT.terms.includes(process.argv[2 + 3]))))) {
     console.log(`Invalid parameters`);
     process.exit(-1);
